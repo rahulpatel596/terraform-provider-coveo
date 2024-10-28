95f56aea099d3bd9b6ea7f7d5cbf42dfdebf997c
rahulpatel <rahulpatel.rjp596@gmail.com>
2024-10-28 16:07:28 -0500

    Configure working

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,11 +1,17 @@
 terraform {
   required_providers {
     coveo = {
-      source = "registry.terraform.io/rahulpatel596/coveo"
+      source  = "registry.terraform.io/rahulpatel596/coveo"
+      version = "1.0.0"
     }
   }
 }
 
-provider "coveo" {}
+provider "coveo" {
+  organization_id = "pwcitlawbs3xfhj3dspx2yh2cbi"
+  api_key = "xx6a366fe8-fe0e-49f0-9c51-d033ad1c2856"
+}
 
-data "coveo_coffees" "example" {}
+resource "coveo_index" "example" {
+  name = "Test Index 95"
+}
